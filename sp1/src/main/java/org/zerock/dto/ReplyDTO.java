594ddb793a65f7db1ba39c7d7bac0ff9dca004b8
wkdwns0501 <britain0501@naver.com>
2026-02-26 16:51:53 +0900

    feat: ReplyDTO 날짜 필드 JSON 포맷 지정 추가

--- sp1/src/main/java/org/zerock/dto/ReplyDTO.java
+++ sp1/src/main/java/org/zerock/dto/ReplyDTO.java
@@ -2,6 +2,8 @@ package org.zerock.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,8 +18,14 @@ public class ReplyDTO {
 	private Long rno;
 	private String replyText;
 	private String replyer;
+	
+	// 자바 객체 <-> JSON 변환 시 데이터의 포맷을 제어
+	// 여기서는 문자열 형태로, yyyy-MM-dd HH:mm:ss 패턴을 사용해서 출력
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime replyDate;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime updateDate;
+	
 	private boolean delFlag;
 	
 	private Long bno;
