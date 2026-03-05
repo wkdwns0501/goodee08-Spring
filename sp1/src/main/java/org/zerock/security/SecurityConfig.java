3d91cfb3408df5d4dcf5fa438f4cfa16326345da
wkdwns0501 <britain0501@naver.com>
2026-03-05 19:40:45 +0900

    feat: Spring Security 설정 클래스 추가

---
+++ sp1/src/main/java/org/zerock/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package org.zerock.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.extern.log4j.Log4j2;
+
+// 이클래스를 스프링 설정 클래스로 등록하고,
+// 내부의 @Bean 메서드들이 반환하는 객체를 스프링 빈으로 스프링 컨테이너에 등록하도록 하는 어노테이션
+@Configuration // 보통 설정 클래스 위에 @Configuration + 메소드 위에 @Bean 세트로 많이 사용
+@Log4j2
+@EnableWebSecurity
+// 스프링 시큐리티의 웹 보안 기능을 활성화시키고, 이클래스에서 정의한 보안 설정을 적용하도록 만드는 어노테이션
+// 이제부터 모든 HTTP 요청(/*)은 시큐리티가 검사
+// SecurityFilterChain을 자동으로 구성, 시큐리티 관련 기본 설정을 등록
+
+// 메소드 단위에서 @PreAuthorize / @PostAuthorize 같은 메소드 보안 어노테이션 활성화
+//@EnableMethodSecurity(prePostEnabled = true) // 예제에서는 XML 설정을 사용
+public class SecurityConfig {
+	
+	@Bean // 빈 등록 어노테이션
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		log.info("-------------- security config --------------");
+		// 이 안에서 필터 체인을 직접 조립해서 리턴하도록 작성
+		
+		// 아이디/비밀번호 기반의 HTML 폼을 통한 로그인 기능을 활성화
+		// 스프링 시큐리티의 기본 로그인 처리 흐름을 구성
+		http.formLogin(config -> {
+			
+		});
+		
+		http.csrf(csrf -> csrf.disable()); // 예제할 때는 번거롭기 때문에 CSRF 완전 비활성화
+		
+		http.exceptionHandling(handler -> {
+			// 403 응답을 직접 제어하는 설정
+			handler.accessDeniedHandler(new Custom403Handler());
+			
+			// 특정 화면으로 보내는 간단한 설정
+//			handler.accessDeniedPage(url);
+		});
+		
+		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(); // (참고) 인자값(int strength)으로 cost factor 넣을 수 있음
+							// cost를 높이면 연산 반복 횟수가 올라감 -> 해시가 다르게 나옴
+							// 즉, salt/cost에 의해 해시값이 결정
+							// 해시를 느리게 설정해서 현실적인 시간 안에 비밀번호를 추측할 수 없게 만듦
+							// 정상 사용자: 0.1초 걸려도 체감 거의 없음
+							// 공격자(해커): 해시가 느릴수록 공격 비용 폭증
+	}
+	
+}
